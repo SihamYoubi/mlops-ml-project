b3f60a286b2bccd903411c085557b5f21f0f7a70
SihamYoubi <youbisiham5@gmail.com>
2026-01-17 21:28:36 +0100

    feat: improve preprocessing pipeline

--- src/features.py
+++ src/features.py
@@ -1,15 +1,15 @@
 # src/features.py
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.impute import SimpleImputer
 
+def _clip(X):
+    # Exemple simple : limite les valeurs entre -3 et 3
+    return X.clip(-3, 3)
+
 def build_numeric_preprocess():
-    """
-    Prétraitement minimal (baseline) :
-    - imputation médiane
-    - standardisation
-    """
     return Pipeline(steps=[
         ("imputer", SimpleImputer(strategy="median")),
         ("scaler", StandardScaler()),
+        ("clip", FunctionTransformer(_clip)), # Nouvelle étape
     ])
